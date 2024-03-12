c83a08e485381113ad4df63de5ff31e65663fdfb
djoenaidi26 <agnestiaagustine260804@gmail.com>
2024-03-12 18:10:29 +0700

    second commit

--- src/week1/luasSegitiga.java
+++ src/week1/luasSegitiga.java
@@ -2,14 +2,12 @@ package week1;
 
 public class luasSegitiga {
     public static void main(String[] args) {
-        // Menghitung luas segitiga siku-siku
         double alas = 6;
         double tinggi = 8;
         double luas = hitungLuasSegitiga(alas, tinggi);
         System.out.println("Luas segitiga siku-siku dengan alas 6 dan tinggi 8 adalah: " + luas);
     }
 
-    // Metode untuk menghitung luas segitiga siku-siku
     public static double hitungLuasSegitiga(double alas, double tinggi) {
         return (alas * tinggi) / 2;
     }
